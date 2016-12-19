ade7c600f7a4eed70f2d0169073ef6b396146fda
Pokey Rule <pokey.rule@gmail.com>
2016-12-19 13:34:55 +0000

    Remove tests

--- tests/test_pubmedasync.py
+++ tests/test_pubmedasync.py
@@ -9,14 +9,7 @@ Tests for `pubmedasync` module.
 """
 
 
-import sys
 import unittest
-from contextlib import contextmanager
-from click.testing import CliRunner
-
-from pubmedasync import pubmedasync
-from pubmedasync import cli
-
 
 
 class TestPubmedasync(unittest.TestCase):
@@ -29,12 +22,3 @@ class TestPubmedasync(unittest.TestCase):
 
     def test_000_something(self):
         pass
-
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'pubmedasync.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
